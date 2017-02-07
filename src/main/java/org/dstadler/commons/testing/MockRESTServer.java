e5fbf01dbca0d7e95c3fc2f037f6182a55c78026
Dominik Stadler <dominik.stadler@gmx.at>
2017-02-07 10:12:52 +0100

    Use blanks instead of tabs

--- src/main/java/org/dstadler/commons/testing/MockRESTServer.java
+++ src/main/java/org/dstadler/commons/testing/MockRESTServer.java
@@ -29,131 +29,131 @@ import org.dstadler.commons.http.NanoHTTPD;
  </code>
  */
 public class MockRESTServer implements Closeable {
-	private static final Logger log = Logger.getLogger(MockRESTServer.class.getName());
+    private static final Logger log = Logger.getLogger(MockRESTServer.class.getName());
 
-	// The range of ports that we try to use for the listening.
-	private static final int PORT_RANGE_START = 15100;
-	private static final int PORT_RANGE_END = 15110;
+    // The range of ports that we try to use for the listening.
+    private static final int PORT_RANGE_START = 15100;
+    private static final int PORT_RANGE_END = 15110;
 
-	private NanoHTTPD httpd;
-	private int port;
+    private NanoHTTPD httpd;
+    private int port;
 
-	/**
-	 * Create a mock server that responds to REST requests with the given HTTP Status Code.
-	 *
-	 * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
-	 * is thrown.
-	 *
-	 * @param status The HTTP status to return, see NanoHTTPD.HTTP_...
-	 * @param mime The mime-type to set for the response, see NanoHTTPD.MIME_...
-	 * @param msg The actual message to return when the HTTP server is called.
-	 * @throws IOException
-	 *             If instantiating the Server failed.
-	 */
-	public MockRESTServer(final String status, final String mime, final String msg) throws IOException {
-		// first try to get the next free port
-		port = getNextFreePort();
+    /**
+     * Create a mock server that responds to REST requests with the given HTTP Status Code.
+     *
+     * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
+     * is thrown.
+     *
+     * @param status The HTTP status to return, see NanoHTTPD.HTTP_...
+     * @param mime The mime-type to set for the response, see NanoHTTPD.MIME_...
+     * @param msg The actual message to return when the HTTP server is called.
+     * @throws IOException
+     *             If instantiating the Server failed.
+     */
+    public MockRESTServer(final String status, final String mime, final String msg) throws IOException {
+        // first try to get the next free port
+        port = getNextFreePort();
 
-		httpd = new NanoHTTPD(port) {
-			/**
-			 * Internal method to provide the response that is set.
-			 */
-			@Override
-			public Response serve(String uri, String method, Properties header, Properties params) {
-				return new NanoHTTPD.Response(status, mime, msg);
-			}
-		};
-	}
+        httpd = new NanoHTTPD(port) {
+            /**
+             * Internal method to provide the response that is set.
+             */
+            @Override
+            public Response serve(String uri, String method, Properties header, Properties params) {
+                return new NanoHTTPD.Response(status, mime, msg);
+            }
+        };
+    }
 
-	/**
-	 * Create a mock server that handles REST requests by running the given Runnable and responding with the given HTTP Status Code.
-	 *
-	 * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
-	 * is thrown.
-	 *
-	 * @param response A {@link Runnable} which is called whenever the HTTP server is called.
-	 * @param status The HTTP status to return, see NanoHTTPD.HTTP_...
-	 * @param mime The mime-type to set for the response, see NanoHTTPD.MIME_...
-	 * @param msg The actual message to return when the HTTP server is called.
-	 * @throws IOException If instantiating the Server failed.
-	 */
-	public MockRESTServer(final Runnable response, final String status, final String mime, final String msg) throws IOException {
-		// first try to get the next free port
-		port = getNextFreePort();
+    /**
+     * Create a mock server that handles REST requests by running the given Runnable and responding with the given HTTP Status Code.
+     *
+     * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
+     * is thrown.
+     *
+     * @param response A {@link Runnable} which is called whenever the HTTP server is called.
+     * @param status The HTTP status to return, see NanoHTTPD.HTTP_...
+     * @param mime The mime-type to set for the response, see NanoHTTPD.MIME_...
+     * @param msg The actual message to return when the HTTP server is called.
+     * @throws IOException If instantiating the Server failed.
+     */
+    public MockRESTServer(final Runnable response, final String status, final String mime, final String msg) throws IOException {
+        // first try to get the next free port
+        port = getNextFreePort();
 
-		httpd = new NanoHTTPD(port) {
-			/**
-			 * Internal method to run the provided Runnable
-			 */
-			@Override
-			public Response serve(String uri, String method, Properties header, Properties params) {
-				response.run();
-				return new NanoHTTPD.Response(status, mime, msg);
-			}
-		};
-	}
+        httpd = new NanoHTTPD(port) {
+            /**
+             * Internal method to run the provided Runnable
+             */
+            @Override
+            public Response serve(String uri, String method, Properties header, Properties params) {
+                response.run();
+                return new NanoHTTPD.Response(status, mime, msg);
+            }
+        };
+    }
 
-	/**
-	 * Create a mock server that responds to REST requests via the given Callable.
-	 *
-	 * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
-	 * is thrown.
-	 *
-	 * @param response A {@link Callable} which is called whenever the HTTP server is called. The returned
-	 *                 {@link org.dstadler.commons.http.NanoHTTPD.Response} contains the HTTP Status Code,
-	 *                 the mime-type and the result-text.
-	 * @throws IOException If instantiating the Server failed.
-	 */
-	public MockRESTServer(final Callable<NanoHTTPD.Response> response) throws IOException {
-		// first try to get the next free port
-		port = getNextFreePort();
+    /**
+     * Create a mock server that responds to REST requests via the given Callable.
+     *
+     * The server tries ports in the range listed above to find one that can be used. If none is usable, a IOException
+     * is thrown.
+     *
+     * @param response A {@link Callable} which is called whenever the HTTP server is called. The returned
+     *                 {@link org.dstadler.commons.http.NanoHTTPD.Response} contains the HTTP Status Code,
+     *                 the mime-type and the result-text.
+     * @throws IOException If instantiating the Server failed.
+     */
+    public MockRESTServer(final Callable<NanoHTTPD.Response> response) throws IOException {
+        // first try to get the next free port
+        port = getNextFreePort();
 
-		httpd = new NanoHTTPD(port) {
-			/**
-			 * Internal method to run the provided Runnable
-			 */
-			@Override
-			public Response serve(String uri, String method, Properties header, Properties params) {
-				try {
-					return response.call();
-				} catch (Exception e) {
-					throw new IllegalStateException(e);
-				}
-			}
-		};
-	}
+        httpd = new NanoHTTPD(port) {
+            /**
+             * Internal method to run the provided Runnable
+             */
+            @Override
+            public Response serve(String uri, String method, Properties header, Properties params) {
+                try {
+                    return response.call();
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
 
-	/**
-	 * Method that is used to find the next available port. It uses the two constants PORT_RANGE_START and
-	 * PORT_RANGE_END defined above to limit the range of ports that are tried.
-	 *
-	 * @return A port number that can be used.
-	 * @throws IOException
-	 *             If no available port is found.
-	 */
+    /**
+     * Method that is used to find the next available port. It uses the two constants PORT_RANGE_START and
+     * PORT_RANGE_END defined above to limit the range of ports that are tried.
+     *
+     * @return A port number that can be used.
+     * @throws IOException
+     *             If no available port is found.
+     */
     private static int getNextFreePort() throws IOException {
-		for (int port = PORT_RANGE_START; port < PORT_RANGE_END; port++) {
-			try {
-			    @SuppressWarnings("resource")
+        for (int port = PORT_RANGE_START; port < PORT_RANGE_END; port++) {
+            try {
+                @SuppressWarnings("resource")
                 ServerSocket sock = new ServerSocket(port);
-				sock.close();
-				//
-				return port;
-			} catch (IOException e) {
-				// seems to be taken, try next one
-				log.warning("Port " + port + " seems to be used already, trying next one: " + e);
-			}
-		}
+                sock.close();
+                //
+                return port;
+            } catch (IOException e) {
+                // seems to be taken, try next one
+                log.warning("Port " + port + " seems to be used already, trying next one: " + e);
+            }
+        }
 
-		throw new IOException("No free port found in the range of [" + PORT_RANGE_START + " - " + PORT_RANGE_END + "]");
-	}
+        throw new IOException("No free port found in the range of [" + PORT_RANGE_START + " - " + PORT_RANGE_END + "]");
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	@Override
-	public void close() throws IOException {
-		httpd.stop();
-	}
+    @Override
+    public void close() throws IOException {
+        httpd.stop();
+    }
 }
--- src/test/java/org/dstadler/commons/testing/MockRESTServerTest.java
+++ src/test/java/org/dstadler/commons/testing/MockRESTServerTest.java
@@ -24,162 +24,162 @@ import org.junit.Test;
 public class MockRESTServerTest {
     private static final Logger log = LoggerFactory.make();
 
-	@After
-	public void tearDown() throws InterruptedException {
-		ThreadTestHelper.waitForThreadToFinishSubstring("NanoHTTP");
-	}
+    @After
+    public void tearDown() throws InterruptedException {
+        ThreadTestHelper.waitForThreadToFinishSubstring("NanoHTTP");
+    }
 
-	@Test
-	public void testLocalhost() throws IOException {
-		runWithHostname("localhost");
-	}
+    @Test
+    public void testLocalhost() throws IOException {
+        runWithHostname("localhost");
+    }
 
-	@Test
-	public void testLocalhostIP() throws IOException {
-		runWithHostname("127.0.0.1");
-	}
+    @Test
+    public void testLocalhostIP() throws IOException {
+        runWithHostname("127.0.0.1");
+    }
 
-	@Test
-	public void testIP() throws IOException {
-		InetAddress localHost = java.net.InetAddress.getLocalHost();
-		assertNotNull("Should get a local address", localHost);
-		String ipAddress = localHost.getHostAddress();
+    @Test
+    public void testIP() throws IOException {
+        InetAddress localHost = java.net.InetAddress.getLocalHost();
+        assertNotNull("Should get a local address", localHost);
+        String ipAddress = localHost.getHostAddress();
 
-		log.info("Had hostname: " + ipAddress + ", address-info: " + localHost);
+        log.info("Had hostname: " + ipAddress + ", address-info: " + localHost);
 
-		assertNotNull("Should get a local ip-address", ipAddress);
-		assertFalse("Local ip-address should not equal localhost", ipAddress.equals("localhost"));
-		// cannot assert on startsWith("127.0.0") as e.g. lab13 reports an ip-address of 127.0.0.2
-		assertFalse("Local ip-address should not equal 127.0.0.1", ipAddress.equals("127.0.0.1"));
+        assertNotNull("Should get a local ip-address", ipAddress);
+        assertFalse("Local ip-address should not equal localhost", ipAddress.equals("localhost"));
+        // cannot assert on startsWith("127.0.0") as e.g. lab13 reports an ip-address of 127.0.0.2
+        assertFalse("Local ip-address should not equal 127.0.0.1", ipAddress.equals("127.0.0.1"));
 
-		runWithHostname(ipAddress);
-	}
+        runWithHostname(ipAddress);
+    }
 
-	@Test
-	public void testHostname() throws IOException {
-		assertNotNull(java.net.InetAddress.getLocalHost());
-		String hostname = java.net.InetAddress.getLocalHost().getHostName();
-		assertNotNull(hostname);
-		assertFalse("Local hostname should not equal localhost", hostname.equals("localhost"));
-		assertFalse("Local hostname should not start with 127.0.0", hostname.startsWith("127.0.0"));
+    @Test
+    public void testHostname() throws IOException {
+        assertNotNull(java.net.InetAddress.getLocalHost());
+        String hostname = java.net.InetAddress.getLocalHost().getHostName();
+        assertNotNull(hostname);
+        assertFalse("Local hostname should not equal localhost", hostname.equals("localhost"));
+        assertFalse("Local hostname should not start with 127.0.0", hostname.startsWith("127.0.0"));
 
-		runWithHostname(hostname);
-	}
+        runWithHostname(hostname);
+    }
 
-	@Test
-	public void testCanonicalHostname() throws IOException {
-		assertNotNull(java.net.InetAddress.getLocalHost());
-		String hostname = java.net.InetAddress.getLocalHost().getCanonicalHostName();
-		assertNotNull(hostname);
-		assertFalse(hostname.equals("localhost"));
-		assertFalse(hostname.startsWith("127.0.0"));
+    @Test
+    public void testCanonicalHostname() throws IOException {
+        assertNotNull(java.net.InetAddress.getLocalHost());
+        String hostname = java.net.InetAddress.getLocalHost().getCanonicalHostName();
+        assertNotNull(hostname);
+        assertFalse(hostname.equals("localhost"));
+        assertFalse(hostname.startsWith("127.0.0"));
 
-		runWithHostname(hostname);
-	}
+        runWithHostname(hostname);
+    }
 
-	private void runWithHostname(String hostname) throws IOException {
-		try (MockRESTServer server = new MockRESTServer(NanoHTTPD.HTTP_OK,  NanoHTTPD.MIME_PLAINTEXT, "OK")) {
-			boolean check = UrlUtils.isAvailable("http://" + hostname + ":" + server.getPort(), false, 500);
-			assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + check, check);
+    private void runWithHostname(String hostname) throws IOException {
+        try (MockRESTServer server = new MockRESTServer(NanoHTTPD.HTTP_OK,  NanoHTTPD.MIME_PLAINTEXT, "OK")) {
+            boolean check = UrlUtils.isAvailable("http://" + hostname + ":" + server.getPort(), false, 500);
+            assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + check, check);
 
-			check = UrlUtils.isAvailable("http://" + hostname + ":" + server.getPort(), true, 500);
-			assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + check, check);
+            check = UrlUtils.isAvailable("http://" + hostname + ":" + server.getPort(), true, 500);
+            assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + check, check);
 
-			String checkStr = UrlUtils.retrieveData("http://" + hostname + ":" + server.getPort(), 500);
-			assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + checkStr, checkStr.length() > 0);
+            String checkStr = UrlUtils.retrieveData("http://" + hostname + ":" + server.getPort(), 500);
+            assertTrue("Host: " + hostname + ":" + server.getPort() + ": Had: " + checkStr, checkStr.length() > 0);
 
-			String data = UrlUtils.retrieveData("http://" + hostname + ":" + server.getPort(), 500);
-			assertEquals("Host: " + hostname + ":" + server.getPort() + ": Had: " + data, "OK", data);
-		}
-	}
+            String data = UrlUtils.retrieveData("http://" + hostname + ":" + server.getPort(), 500);
+            assertEquals("Host: " + hostname + ":" + server.getPort() + ": Had: " + data, "OK", data);
+        }
+    }
 
-	@Test
-	public void testStartupTwice() throws IOException {
-		try (MockRESTServer server = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>")) {
-			try (MockRESTServer server2 = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>2</html>")) {
-				assertTrue(server.getPort() != server2.getPort());
+    @Test
+    public void testStartupTwice() throws IOException {
+        try (MockRESTServer server = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>")) {
+            try (MockRESTServer server2 = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>2</html>")) {
+                assertTrue(server.getPort() != server2.getPort());
 
-				String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
-				assertEquals("<html>1</html>", data);
+                String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
+                assertEquals("<html>1</html>", data);
 
-				data = UrlUtils.retrieveData("http://localhost:" + server2.getPort(), 10_000);
-				assertEquals("<html>2</html>", data);
-			}
-		}
-	}
+                data = UrlUtils.retrieveData("http://localhost:" + server2.getPort(), 10_000);
+                assertEquals("<html>2</html>", data);
+            }
+        }
+    }
 
-	@Test
-	public void testExhaustPorts() throws IOException {
-		MockRESTServer[] servers = new MockRESTServer[100];
-		try {
-			for (int i = 0; i < 100; i++) {
-				servers[i] = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>" + i + "</html>");
-			}
-		} catch (IOException e) {
-			TestHelpers.assertContains(e, "No free port found");
-		}
+    @Test
+    public void testExhaustPorts() throws IOException {
+        MockRESTServer[] servers = new MockRESTServer[100];
+        try {
+            for (int i = 0; i < 100; i++) {
+                servers[i] = new MockRESTServer(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>" + i + "</html>");
+            }
+        } catch (IOException e) {
+            TestHelpers.assertContains(e, "No free port found");
+        }
 
-		for(int i = 0;i < 100;i++) {
-			if(servers[i] != null) {
-				servers[i].close();
-			}
-		}
-	}
+        for(int i = 0;i < 100;i++) {
+            if(servers[i] != null) {
+                servers[i].close();
+            }
+        }
+    }
 
-	@Test
-	public void testWithRunnable() throws IOException {
-		final AtomicBoolean called = new AtomicBoolean();
-		try (MockRESTServer server = new MockRESTServer(new Runnable() {
-			@Override
-			public void run() {
-				assertFalse("Should be called exactly once, but was already called before", called.get());
-				called.set(true);
-			}
-		}, NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>")) {
-			String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
-			assertEquals("<html>1</html>", data);
-		}
+    @Test
+    public void testWithRunnable() throws IOException {
+        final AtomicBoolean called = new AtomicBoolean();
+        try (MockRESTServer server = new MockRESTServer(new Runnable() {
+            @Override
+            public void run() {
+                assertFalse("Should be called exactly once, but was already called before", called.get());
+                called.set(true);
+            }
+        }, NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>")) {
+            String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
+            assertEquals("<html>1</html>", data);
+        }
 
-		assertTrue("Should be called now", called.get());
-	}
+        assertTrue("Should be called now", called.get());
+    }
 
-	@Test
-	public void testWithCallable() throws IOException {
-		final AtomicBoolean called = new AtomicBoolean();
-		try (MockRESTServer server = new MockRESTServer(new Callable<NanoHTTPD.Response>() {
-			@Override
-			public NanoHTTPD.Response call() {
-				assertFalse("Should be called exactly once, but was already called before", called.get());
-				called.set(true);
-				return new NanoHTTPD.Response(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>");
-			}
-		})) {
-			String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
-			assertEquals("<html>1</html>", data);
-		}
+    @Test
+    public void testWithCallable() throws IOException {
+        final AtomicBoolean called = new AtomicBoolean();
+        try (MockRESTServer server = new MockRESTServer(new Callable<NanoHTTPD.Response>() {
+            @Override
+            public NanoHTTPD.Response call() {
+                assertFalse("Should be called exactly once, but was already called before", called.get());
+                called.set(true);
+                return new NanoHTTPD.Response(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_HTML, "<html>1</html>");
+            }
+        })) {
+            String data = UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
+            assertEquals("<html>1</html>", data);
+        }
 
-		assertTrue("Should be called now", called.get());
-	}
+        assertTrue("Should be called now", called.get());
+    }
 
-	@Test
-	public void testWithCallableException() throws IOException {
-		final AtomicBoolean called = new AtomicBoolean();
-		try (MockRESTServer server = new MockRESTServer(new Callable<NanoHTTPD.Response>() {
-			@Override
-			public NanoHTTPD.Response call() {
-				assertFalse("Should be called exactly once, but was already called before", called.get());
-				called.set(true);
-				throw new RuntimeException("TestException");
-			}
-		})) {
-			try {
-				UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
-				fail("Should catch HTTP 500 here because of the exception");
-			} catch (IOException e) {
-				TestHelpers.assertContains(e, "Error 500");
-			}
-		}
+    @Test
+    public void testWithCallableException() throws IOException {
+        final AtomicBoolean called = new AtomicBoolean();
+        try (MockRESTServer server = new MockRESTServer(new Callable<NanoHTTPD.Response>() {
+            @Override
+            public NanoHTTPD.Response call() {
+                assertFalse("Should be called exactly once, but was already called before", called.get());
+                called.set(true);
+                throw new RuntimeException("TestException");
+            }
+        })) {
+            try {
+                UrlUtils.retrieveData("http://localhost:" + server.getPort(), 10_000);
+                fail("Should catch HTTP 500 here because of the exception");
+            } catch (IOException e) {
+                TestHelpers.assertContains(e, "Error 500");
+            }
+        }
 
-		assertTrue("Should be called now", called.get());
-	}
+        assertTrue("Should be called now", called.get());
+    }
 }
